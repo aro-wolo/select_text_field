77b7cad8d8fc286b05128fe55cb64c4edbef262f
Arowolo Abiodun <aarowolom@gmail.com>
2024-03-15 12:33:58 +0100

    build 🚀

---
+++ example/example.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:select_text_field/select_text_field.dart';
+
+/// Complete Simple Example
+class Example extends StatefulWidget {
+  const Example({Key? key}) : super(key: key);
+
+  @override
+  _ExampleState createState() => _ExampleState();
+}
+
+class _ExampleState extends State<Example> {
+  // simple check
+
+  // form key for validation
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> _genderList = ["Male", "Female"];
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Select Text Field"),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SelectTextField(
+              options: _genderList,
+              label: 'Gender',
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
